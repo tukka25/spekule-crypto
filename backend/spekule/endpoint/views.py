91164e1e2f441d7885c30d469236ff76e29edad9
tukka25 <aboodytukka@gmail.com>
2024-05-07 01:41:09 +0400

    added interval class and some parsing to tradingView

--- backend/spekule/endpoint/views.py
+++ backend/spekule/endpoint/views.py
@@ -10,7 +10,9 @@ from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from tradingview_ta import TA_Handler, Interval, Exchange
+from tradingview_ta import *
 from django.contrib.auth.models import User
+from .tradingViewUtils import Interval
 import requests , re, json, random , string
 
 @csrf_exempt
@@ -33,13 +35,28 @@ def tradingView(request):
 	if (request.method != "POST"):
 		return (JsonResponse({'status':'unauthorized'}, status=401))
 	try:
+		body_json = json.loads(request.body)
+		# print(f"body = {len(body_json)}")
+		if (len(body_json) != 4):
+			return (JsonResponse({'status' : '4 arguments should be sent'}, status=400))
+		req_symbol = body_json["symbol"]
+		print(f"symbols = {req_symbol}")
+		req_screener = body_json["screener"]
+		req_exchange = body_json["exchange"]
+		req_interval = body_json["interval"]
+		print(f"interval = {req_interval}")
+		if (Interval.get_interval(req_interval) == ""):
+			return (JsonResponse({'status': 'Unknown Interval'}, status=400))
+		# if (len(req_symbol) > 1):
+		# 	analysis = get_multiple_analysis(screener=req_screener, interval=req_interval, symbols=req_symbol)
 		tesla = TA_Handler (
-		symbol="ETH",
-		screener="america",
-		exchange="NASDAQ",
-		interval=Interval.INTERVAL_1_DAY
+			symbol=req_symbol,
+			screener=req_screener,
+			exchange=req_exchange,
+			interval=req_interval
 		)
 		print(tesla.get_analysis().summary)
-		return (JsonResponse({'status':'success'}, status=200))
+		return (JsonResponse({'status':'success', 'summary': tesla.get_analysis().summary}, status=200))
 	except Exception as err:
+		print(str(err))
 		return (JsonResponse({'status': str(err)}, status=401))
---
+++ test.py
@@ -0,0 +1,20 @@
+class Interval:
+    intervals = {
+		 "1m" : "INTERVAL_1_MINUTE",
+		 "5m" : "INTERVAL_5_MINUTES",
+		 "15m" : "INTERVAL_15_MINUTES",
+		 "30m" : "INTERVAL_30_MINUTES",
+		 "1h" : "INTERVAL_1_HOUR",
+		 "2h" : "INTERVAL_2_HOURS",
+		 "4h" : "INTERVAL_4_HOURS",
+		 "1d" : "INTERVAL_1_DAY",
+		 "1W" : "INTERVAL_1_WEEK",
+		 "1M" : "INTERVAL_1_MONTH"
+	}
+
+    @classmethod
+    def get_interval(cls, key):
+      return cls.intervals.get(key, "Unknown interval")
+
+print(Interval.get_interval("1h")) 
+# print(Interval.get_interval("1s")) 
